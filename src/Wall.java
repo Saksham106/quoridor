6f06b8f69a596cc7921b627b3ad8bea118f739ab
edaadazman <edaad@bu.edu>
2025-10-26 13:50:42 -0400

    improved and simplififed wall junction crossing and overalpping logic

--- src/Wall.java
+++ src/Wall.java
@@ -15,7 +15,7 @@ public class Wall extends Piece {
         public char getSymbol() {
             return symbol;
         }
-        
+
         public static Orientation fromChar(char c) {
             char upperC = Character.toUpperCase(c);
             for (Orientation orientation : values()) {
@@ -61,16 +61,8 @@ public class Wall extends Piece {
             // Same orientation - check if they're in the same position
             return this.row == other.row && this.col == other.col;
         } else {
-            // Different orientations - check for intersection
-            if (this.orientation == Orientation.HORIZONTAL) {
-                // This wall is horizontal, other is vertical
-                return (this.row == other.row || this.row == other.row + 1) &&
-                       (this.col == other.col || this.col + 1 == other.col);
-            } else {
-                // This wall is vertical, other is horizontal
-                return (this.col == other.col || this.col == other.col + 1) &&
-                       (this.row == other.row || this.row + 1 == other.row);
-            }
+
+            return this.row == other.row && this.col == other.col;
         }
     }
 
